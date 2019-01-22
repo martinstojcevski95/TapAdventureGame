14b2f0c57ee71c40bc3189b06319e0e49c892ec0
martinstojcevski95 <stojcevski.m95@gmail.com>
2019-01-22 23:08:41 +0100

    v_04
    Mine button

--- TapAdventureGame/Assets/Scripts/SilverPerClick.cs
+++ TapAdventureGame/Assets/Scripts/SilverPerClick.cs
@@ -11,18 +11,46 @@ public class SilverPerClick : MonoBehaviour
     [SerializeField] Text spc;
     public float iron = 5;
     public float ironPerClick = 1f;
-
+     public float mineWaitTime;
+     public float oldMineWaitTime;
+    [SerializeField] Color affordableColor,waitingColor;
+    Slider tempSlider;
+    [SerializeField] Button MinePerClick;
+    bool canStart;
+ 
     private void Start()
     {
-        iron = PlayerPrefs.GetFloat("I");
-        ironPerClick = PlayerPrefs.GetFloat("IPC");
+
+            iron = PlayerPrefs.GetFloat("I");
+            ironPerClick = PlayerPrefs.GetFloat("IPC");
+        mineWaitTime = PlayerPrefs.GetFloat("time");
     }
 
     // Update is called once per frame
     void Update()
     {
         UpdateInfo();
-      
+
+
+        if (canStart)
+        {
+            if (oldMineWaitTime <= mineWaitTime)
+            {
+                MinePerClick.interactable = false;
+                oldMineWaitTime += Time.deltaTime;
+                MinePerClick.GetComponentInChildren<Text>().text = "On Cooldown...";
+                MinePerClick.GetComponent<Image>().color = waitingColor;
+            }
+            else
+            {
+                MinePerClick.interactable = true;
+                canStart = false;
+                MinePerClick.GetComponentInChildren<Text>().text = "Mine";
+                MinePerClick.GetComponent<Image>().color = affordableColor;
+
+            }
+        }
+
     }
 
     void UpdateInfo()
@@ -36,9 +64,14 @@ public class SilverPerClick : MonoBehaviour
         PlayerPrefs.SetFloat("I", iron);
     }
 
+ 
     public void OnClick()
     {
         iron += ironPerClick;
-        Debug.Log(iron);
+        Debug.Log("iron " + iron);
+        oldMineWaitTime = 0;
+        mineWaitTime += 0.25f;
+        PlayerPrefs.SetFloat("time", mineWaitTime);
+        canStart = true;
     }
 }
